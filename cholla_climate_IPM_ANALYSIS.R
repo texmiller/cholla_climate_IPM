5b38de7cf39e3afc517f57cbf92cc5af4bccb50e
unknown <tom.miller@rice.edu>
2019-06-18 10:36:45 -0500

    Updated so that misc parameters are incorporated into main JAGS output. Also, added neat counters to lambda-PC plot.

--- cholla_climate_IPM_ANALYSIS.R
+++ cholla_climate_IPM_ANALYSIS.R
@@ -14,7 +14,7 @@ mat.size = 200
 cholla_mean <- bigmatrix(params = mean_params,
           PC1 = c(0,0), PC2 = c(0,0), PC3 = c(0,0),
           random = F, 
-          lower.extension = -.2, 
+          lower.extension = -0.2, 
           upper.extension = 1,
           mat.size = mat.size)
 lambda(cholla_mean$IPMmat)
@@ -120,17 +120,8 @@ for(i in 1:n_post){
   sample.params$surv.bclim <- params_post[rand.indices[i],] %>% 
     select(surv.bclim.1.1.:surv.bclim.3.3.) %>% 
     matrix(nrow=3) 
-  
-  sample.params$seedsperfruit <- mean_params$seedsperfruit
-  sample.params$seedsurv0yr <- mean_params$seedsurv0yr
-  sample.params$germ1yo <- mean_params$germ1yo
-  sample.params$germ2yo <- mean_params$germ2yo
-  sample.params$precensus.surv <- mean_params$precensus.surv
-  sample.params$sdling.size.mean <- mean_params$sdling.size.mean
-  sample.params$sdling.size.sd <- mean_params$sdling.size.sd
   sample.params$min.size <- mean_params$min.size
-  sample.params$max.size <- mean_params$max.size
-  
+  sample.params$max.size <- mean_params$max.siz
   
   for(j in 1:length(x_PC1)){
     lambda_PC1_post[i,j]<-lambda(bigmatrix(params = sample.params,
@@ -162,18 +153,30 @@ for(i in 1:n_post){
 #write.csv(lambda_PC1_post,"lambda_PC1_post.csv")
 #write.csv(lambda_PC2_post,"lambda_PC2_post.csv")
 #write.csv(lambda_PC3_post,"lambda_PC3_post.csv")
+lambda_PC1_post <- read.csv("lambda_PC1_post.csv")
+lambda_PC2_post <- read.csv("lambda_PC2_post.csv")
+lambda_PC3_post <- read.csv("lambda_PC3_post.csv")
 
-PC1.lambda.CI<-matrix(0,2,length(x_PC1))
+PC1.lambda.95<-PC1.lambda.75<-PC1.lambda.50<-PC1.lambda.25<-matrix(0,2,length(x_PC1))
 for(j in 1:length(x_PC1)){
-  PC1.lambda.CI[,j]<-quantile(lambda_PC1_post[,j],probs=c(0.025,0.975))
+  PC1.lambda.95[,j]<-quantile(lambda_PC1_post[,j],probs=c(0.025,0.975))
+  PC1.lambda.75[,j]<-quantile(lambda_PC1_post[,j],probs=c(0.125,0.875))
+  PC1.lambda.50[,j]<-quantile(lambda_PC1_post[,j],probs=c(0.25,0.75))
+  PC1.lambda.25[,j]<-quantile(lambda_PC1_post[,j],probs=c(0.375,0.625))
 }
-PC2.lambda.CI<-matrix(0,2,length(x_PC2))
+PC2.lambda.95<-PC2.lambda.75<-PC2.lambda.50<-PC2.lambda.25<-matrix(0,2,length(x_PC2))
 for(j in 1:length(x_PC2)){
-  PC2.lambda.CI[,j]<-quantile(lambda_PC2_post[,j],probs=c(0.025,0.975))
+  PC2.lambda.95[,j]<-quantile(lambda_PC2_post[,j],probs=c(0.025,0.975))
+  PC2.lambda.75[,j]<-quantile(lambda_PC2_post[,j],probs=c(0.125,0.875))
+  PC2.lambda.50[,j]<-quantile(lambda_PC2_post[,j],probs=c(0.25,0.75))
+  PC2.lambda.25[,j]<-quantile(lambda_PC2_post[,j],probs=c(0.375,0.625))
 }
-PC3.lambda.CI<-matrix(0,2,length(x_PC3))
+PC3.lambda.95<-PC3.lambda.75<-PC3.lambda.50<-PC3.lambda.25<-matrix(0,2,length(x_PC3))
 for(j in 1:length(x_PC3)){
-  PC3.lambda.CI[,j]<-quantile(lambda_PC3_post[,j],probs=c(0.025,0.975))
+  PC3.lambda.95[,j]<-quantile(lambda_PC3_post[,j],probs=c(0.025,0.975))
+  PC3.lambda.75[,j]<-quantile(lambda_PC3_post[,j],probs=c(0.125,0.875))
+  PC3.lambda.50[,j]<-quantile(lambda_PC3_post[,j],probs=c(0.25,0.75))
+  PC3.lambda.25[,j]<-quantile(lambda_PC3_post[,j],probs=c(0.375,0.625))
 }
 
 
@@ -182,7 +185,16 @@ par(mfrow=c(1,3),mar=c(5,5,2,1))
 plot(x_PC1,lambda_PC1,type="n",lwd=4,ylim=c(0.7,1),
      ylab=expression(paste(lambda)),xlab="Climate PC 1",cex.lab=1.6)
 polygon(x=c(x_PC1,rev(x_PC1)),
-        y=c(PC1.lambda.CI[1,],rev(PC1.lambda.CI[2,])),
+        y=c(PC1.lambda.95[1,],rev(PC1.lambda.95[2,])),
+        col=alpha("black",0.05),border=NA)
+polygon(x=c(x_PC1,rev(x_PC1)),
+        y=c(PC1.lambda.75[1,],rev(PC1.lambda.75[2,])),
+        col=alpha("black",0.05),border=NA)
+polygon(x=c(x_PC1,rev(x_PC1)),
+        y=c(PC1.lambda.50[1,],rev(PC1.lambda.50[2,])),
+        col=alpha("black",0.05),border=NA)
+polygon(x=c(x_PC1,rev(x_PC1)),
+        y=c(PC1.lambda.25[1,],rev(PC1.lambda.25[2,])),
         col=alpha("black",0.05),border=NA)
 lines(x_PC1,lambda_PC1,lwd=2)
 points(coef(PC1modB)[1]+coef(PC1modB)[2]*1970,lambda_PC1[which.min(abs(x_PC1-(coef(PC1modB)[1]+coef(PC1modB)[2]*1970)))],
@@ -199,7 +211,16 @@ title("A",font=3,adj=0)
 plot(x_PC2,lambda_PC2,type="n",lwd=4,ylim=c(0.7,1),
      ylab=expression(paste(lambda)),xlab="Climate PC 2",cex.lab=1.6)
 polygon(x=c(x_PC2,rev(x_PC2)),
-        y=c(PC2.lambda.CI[1,],rev(PC2.lambda.CI[2,])),
+        y=c(PC2.lambda.95[1,],rev(PC2.lambda.95[2,])),
+        col=alpha("black",0.05),border=NA)
+polygon(x=c(x_PC2,rev(x_PC2)),
+        y=c(PC2.lambda.75[1,],rev(PC2.lambda.75[2,])),
+        col=alpha("black",0.05),border=NA)
+polygon(x=c(x_PC2,rev(x_PC2)),
+        y=c(PC2.lambda.50[1,],rev(PC2.lambda.50[2,])),
+        col=alpha("black",0.05),border=NA)
+polygon(x=c(x_PC2,rev(x_PC2)),
+        y=c(PC2.lambda.25[1,],rev(PC2.lambda.25[2,])),
         col=alpha("black",0.05),border=NA)
 lines(x_PC2,lambda_PC2,lwd=2)
 points(coef(PC2modB)[1]+coef(PC2modB)[2]*1970,lambda_PC2[which.min(abs(x_PC2-(coef(PC2modB)[1]+coef(PC2modB)[2]*1970)))],
@@ -216,7 +237,16 @@ title("B",font=3,adj=0)
 plot(x_PC3,lambda_PC3,type="n",lwd=4,ylim=c(0.7,1),
      ylab=expression(paste(lambda)),xlab="Climate PC 3",cex.lab=1.6)
 polygon(x=c(x_PC3,rev(x_PC3)),
-        y=c(PC3.lambda.CI[1,],rev(PC3.lambda.CI[2,])),
+        y=c(PC3.lambda.95[1,],rev(PC3.lambda.95[2,])),
+        col=alpha("black",0.05),border=NA)
+polygon(x=c(x_PC3,rev(x_PC3)),
+        y=c(PC3.lambda.75[1,],rev(PC3.lambda.75[2,])),
+        col=alpha("black",0.05),border=NA)
+polygon(x=c(x_PC3,rev(x_PC3)),
+        y=c(PC3.lambda.50[1,],rev(PC3.lambda.50[2,])),
+        col=alpha("black",0.05),border=NA)
+polygon(x=c(x_PC3,rev(x_PC3)),
+        y=c(PC3.lambda.25[1,],rev(PC3.lambda.25[2,])),
         col=alpha("black",0.05),border=NA)
 lines(x_PC3,lambda_PC3,lwd=2)
 points(coef(PC3modB)[1]+coef(PC3modB)[2]*1970,lambda_PC3[which.min(abs(x_PC3-(coef(PC3modB)[1]+coef(PC3modB)[2]*1970)))],
