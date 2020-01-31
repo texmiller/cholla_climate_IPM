27f7d2f9913d1c119f4aa0f63f08dbb3655afee5
unknown <tom.miller@rice.edu>
2020-01-31 12:01:33 -0500

    Created new script to run the reanalysis suggested by R2

---
+++ reanalysis_no_extrapolation.R
@@ -0,0 +1,4 @@
+## re-runs the demographic analysis with no extrapolation into unobserved climates
+## this is using the vital rate models from the original submission
+source("cholla_climate_IPM_SOURCE.R")
+
